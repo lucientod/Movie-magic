bfb691cf3a969761ae119e1b5bf0174b345d7a3b
Lucien Todorov <lucientod@gmail.com>
2024-02-02 16:43:33 +0200

    create homepage

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const express = require(`express`)
+const handlebars = require('express-handlebars')
+const path = require('path')
 
 const app = express()
 const port = 5000
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static('./src/public'))
+
 app.get('/', (req,res)=>{
-    res.send(`<h1>Hello there</h1>`)
+    res.render('home', {layout:false})
 })
 
 app.listen(port, ()=>console.log(`The server is listening on port ${port}...`))
