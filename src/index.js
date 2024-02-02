b179492683042371f0ceeb7b652cea431b807190
Lucien Todorov <lucientod@gmail.com>
2024-02-02 17:17:15 +0200

    add about page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require(`express`)
 const handlebars = require('express-handlebars')
 const path = require('path')
+const routes = require('./routes.js')
 
 const app = express()
 const port = 5000
@@ -12,8 +13,7 @@ app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static('./src/public'))
 
-app.get('/', (req,res)=>{
-    res.render('home', {layout:false})
-})
+app.use(routes)
+
 
 app.listen(port, ()=>console.log(`The server is listening on port ${port}...`))
