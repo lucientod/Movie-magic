809ed40287413caa594497e9b44aa3ea69668859
Lucien Todorov <lucientod@gmail.com>
2024-02-02 17:27:55 +0200

    adding 404 page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,12 +1,16 @@
 const router = require('express').Router()
 
 
-router.get('/', (req,res)=>{
+router.get('/', (req, res) => {
     res.render('home')
 })
 
-router.get('/about', (req,res)=>{
+router.get('/about', (req, res) => {
     res.render('about')
+})
+
+router.get('/404', (req, res) => {
+    res.render('404')
 })
 
 module.exports = router
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,10 @@
 const router = require('express').Router()
 const homeController = require('./controllers/homeController.js')
+const movieController = require('./controllers/movieController.js')
 
 router.use(homeController)
-
+router.use(movieController)
+router.get('*',(req,res)=>{
+    res.redirect('/404')
+})
 module.exports = router
