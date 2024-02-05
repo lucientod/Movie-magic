0fe459ca97de25a9a013e56edad9325bc91c3c3a
Lucien Todorov <lucientod@gmail.com>
2024-02-05 07:12:15 +0200

    add create movie

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
-
+const movieService = require('../services/movieService.js')
 
 router.get('/', (req, res) => {
-    res.render('home')
+    const movies = movieService.getAll()
+    res.render('home', { movies })
 })
 
 router.get('/about', (req, res) => {
--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,12 +1,20 @@
 const router = require('express').Router()
+const movieService = require('../services/movieService.js')
 
 router.get('/create', (req, res) => {
     res.render('create')
 })
 
 router.post('/create', (req, res) => {
-    res.send('creating.......')
-    console.log('did it');
+    const newMovie = req.body
+    movieService.create(newMovie)
+    res.redirect('/')
 })
 
+router.get('/details:movieID', (req, res) => {
+    const movieID = req.params.movieID
+    const movie = movieService.getOne(movieID)
+    movie.rating = new Array(Number(movie.rating)).fill(true);
+    res.render('details', { movie })
+})
 module.exports = router
