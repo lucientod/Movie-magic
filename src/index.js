fbe127a9318e42694725e5135f4c9c187b64ed0a
Lucien Todorov <lucientod@gmail.com>
2024-02-08 20:00:13 +0200

    mongoosh

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require(`express`)
+const mongoose = require('mongoose')
 
 const routes = require('./routes.js')
 const configHandlebars = require('./config/configHandlebars.js')
@@ -12,4 +13,10 @@ configExpress(app)
 
 app.use(routes)
 
-app.listen(port, () => console.log(`The server is listening on port ${port}...`))
+mongoose.connect(`mongodb://localhost:27017/magic-movies`)
+    .then(() => {
+        console.log('DB connected');
+
+        app.listen(port, () => console.log(`The server is listening on port ${port}...`))
+    })
+    .catch(err => console.log('Cannoct connect to DB'))
