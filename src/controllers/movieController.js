69964f0db24f281c9c611a96e8be2fc37c9e41ec
Lucien Todorov <lucientod@gmail.com>
2024-02-12 15:18:05 +0200

    add attach page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const movieService = require('../services/movieService.js')
+const castService = require('../services/castService.js')
 
 router.get('/create', (req, res) => {
     res.render('create')
@@ -9,8 +10,8 @@ router.post('/create', async (req, res) => {
     const newMovie = req.body
     console.log(newMovie);
     try {
-      await  movieService.create(newMovie)        
-      res.redirect('/')
+        await movieService.create(newMovie)
+        res.redirect('/')
     } catch (error) {
         console.log(error.message);
         res.redirect('/create')
@@ -27,4 +28,19 @@ router.get('/details/:movieID', async (req, res) => {
     movie.rating = new Array(movie.rating).fill(true)
     res.render('details', { movie })
 })
+
+router.get('/details/:movieId/attach', async (req, res) => {
+    const movie = await movieService.getOne(req.params.movieId)
+    const casts = await castService.getAll().lean()
+    res.render('details/attach', { ...movie, casts })
+})
+
+router.post('details/:movieId/attach', async (req, res) => {
+    const castId = req.body.cast;
+    console.log(castId);
+    const movieId = req.params.movieId
+    await movieService.attach(movieId, castId)
+    res.redirect(`/details/${movieId}/attach`)
+})
+
 module.exports = router
